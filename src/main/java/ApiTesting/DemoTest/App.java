f0438710b266ae2770c9343284f2e4fc03eaf077
Yogendra Thakur <ythakur@zscaler.com>
2022-05-29 22:33:43 +0530

    Api Testing Commit

---
+++ src/main/java/ApiTesting/DemoTest/App.java
@@ -0,0 +1,16 @@
+package ApiTesting.DemoTest;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(LogStatus.INFO);
+    }
+}
